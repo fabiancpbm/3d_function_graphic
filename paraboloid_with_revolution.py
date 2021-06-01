b8cfe1ce0c65c1c3a02b04a487056bd41630f16c
Fabian <fabiancpbm@gmail.com>
2021-06-01 20:48:44 -0300

    Ajustando as cores.

--- paraboloid_with_revolution.py
+++ paraboloid_with_revolution.py
@@ -3,8 +3,8 @@ from OpenGL.GLU import *
 from OpenGL.GL import *
 import math
 
-n1 = 150
-n2 = 150
+n1 = 50
+n2 = 50
 r = 2
 a = 0
 
@@ -23,14 +23,17 @@ def draw():
     glPushMatrix() # Guarda o estado de transformação da matriz atual (matriz M)
     
     # glRotatef(angulo, x, y, z)
-    glRotatef(a,0.0,0.0,1.0) # Fazendo matriz atual N = M * rotação
-    for i in range(0, n1): #Variação do teta
-        #glBegin(GL_POINTS)
+    glRotatef(a,1.0,1.0,1.0) # Fazendo matriz atual N = M * rotação
+    for i in range(0, round(n1/2)): #Variação do teta
         glBegin(GL_QUAD_STRIP)
-        glColor3fv((i/n1,i/n1,0))
         for j in range(0, n2): # Variação do phi
             x, y, z = f(i, j)
             x1, y1, z1 = f(i + 1, j)
+
+            red = 1
+            green = 1-((i+1)/(n1-1))
+            blue = 0
+            glColor3fv((red, green, blue))
             glVertex3f(x, y, z)
             glVertex3f(x1, y1, z1)
         glEnd()
@@ -45,12 +48,12 @@ def timer(i):
 glutInit(sys.argv)
 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE)
 glutInitWindowSize(800, 600)
-glutCreateWindow("Bola")
+glutCreateWindow("Paraboloid with revolution")
 glutDisplayFunc(draw)
 glEnable(GL_MULTISAMPLE)
 glEnable(GL_DEPTH_TEST)
 glClearColor(0, 0, 0, 1)
 gluPerspective(45, 800.0 / 600.0, 0.1, 100.0) # Aplica matriz perspectiva ao universa
-glTranslatef(0.0, 0.0, -20) # Empurrando 20 und para dentro da tela do PC
+glTranslatef(0.0, -1.0, -10) # Empurrando 20 und para dentro da tela do PC
 glutTimerFunc(10, timer, 1)
 glutMainLoop()
